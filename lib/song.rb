29a878c2d90eb4aca5ee5d9f288289549744ac35
Justin Pierce <jwpierce1984@gmail.com>
2020-02-03 17:09:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,24 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ 
   def self.all
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
-  def self.create
-    song = song.new
-    song.save
-    song
-  end
-  def self.new_by_name (song_name)
-    song = self.new
-    song.name = song_name
-    song
+  
+  def self.create (song)
+    self.class.all << song
   end
   
-    
+  def self.new_by_name(song_name)
+    name = song_name
+    self.class.all << name
+  end
+  
+  
+  
 end
