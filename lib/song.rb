7d8805cb187283b8c19b9d0203afb2dc101ea46c
Justin Pierce <jwpierce1984@gmail.com>
2020-02-03 18:58:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
   def self.new_from_filename(filename)
     parts = filename.split (" - ")
     artist_name = parts[0]
-    song_name = parts[1].
+    song_name = parts[1].gsub
     song = self.new
     song.name = song_name
     song.artist_name = artist_name
